b1f3152f953228e551514cb1fa372272f9508e3c
hariomfw21 <darkphinixgamer@gmail.com>
2023-06-26 02:39:29 +0530

    backend

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,12 +1,11 @@
 const jwt = require("jsonwebtoken");
 
 const auth = (req, res, next) => {
-    const token = req.headers.authorization.split(' ')[1];
-    console.log(token);
     if (!token) {
         return res.status(403).send({ message: 'you must be logged in' }); // return here
     }
-
+    const token = req.headers.authorization.split(' ')[1];
+    console.log(token);
     jwt.verify(token,process.env.SECRET_KEY, (err,data)=>{
         if(err){
             return res.status(403).send({ message: 'you must be logged in again' }); // return here
